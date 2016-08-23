78ae0f5bd69115b1e16be0b1d4c790a57d2e50cb
Ændrew Rininsland <andrew.rininsland@ft.com>
2016-08-23 16:35:46 +0100

    Should be working now

--- lib/index.ts
+++ lib/index.ts
@@ -11,38 +11,68 @@ import * as mount from 'koa-mount';
 import * as serve from 'koa-static';
 import * as chalk from 'chalk';
 import * as convert from 'koa-convert';
+import * as compose from 'koa-compose';
 import * as koaBS from 'koa-browser-sync';
 import { resolve } from 'path';
 
 import router from './routes';
 
-const app = new Koa();
-const port = process.env.PORT || 5555;
-const viewsPath = resolve(__dirname, '..', '/views');
-const browserSync = convert(koaBS({
-  init: true,
-  logLevel: 'silent',
-  files: resolve(process.env.PWD, '**/*'),
-  notify: true,
-  open: true,
-  ui: true,
-  ghostMode: true,
-}));
+function framery(opts: FrameryOptions) {
+  const app = new Koa();
+  const port = process.env.PORT || 5555;
+  const viewsPath = resolve(__dirname, '../views');
+  const pkg = require(resolve(__dirname, '../package.json'));
 
+  const browserSync = convert(koaBS({
+    init: true,
+    logLevel: 'silent',
+    files: resolve(process.env.PWD, '**/*'),
+    notify: true,
+    open: true,
+    ui: false,
+    ghostMode: true,
+  }));
 
-app.use(browserSync)
-  .use(bodyParser())
-  .use(views(viewsPath, {
-    extension: 'njk',
-    map: {
-      html: 'nunjucks'
-    }
-  }))
- .use(serve(resolve(__dirname, '..', 'node_modules/', 'graphic-frame')))
- .use(mount('project/', process.env.PWD))
- .use(router.routes())
- .use(router.allowedMethods());
+  const files = compose([
+    serve(resolve(__dirname, '..', 'node_modules/', 'graphic-frame')),
+    serve(process.cwd())
+  ]);
 
-app.listen(port, () => console.log(chalk.black.bgGreen.bold(`Listening on port ${port}`)));
+  app.use(browserSync)
+    .use(function(ctx: CliContext, next: any){
+      ctx.cli = opts;
 
-export default app;
+      return next();
+    })
+    .use(bodyParser())
+    .use(views(viewsPath, {
+      extension: 'njk',
+      map: {
+        njk: 'nunjucks',
+        html: 'nunjucks',
+      }
+    }))
+    .use(router.routes())
+    .use(mount('/', files))
+    .use(router.allowedMethods());
+
+  const txt = chalk.black.bgGreen;
+  const txtRed = chalk.red.bgGreen;
+  const boldTxt = chalk.black.bgGreen.bold;
+
+  return app.listen(port, () => {
+    console.info(boldTxt(`»» Framery version ${pkg.version} ««`));
+    console.info(`${txt('» Project being served @ ')}${txtRed('http://localhost:' + port)}`);
+  });
+}
+
+interface FrameryOptions {
+  'd3-version': string;
+  'use-wrapper': boolean;
+}
+
+interface CliContext extends Koa.Context {
+  cli: any;
+}
+
+module.exports = framery;
--- lib/routes.ts
+++ lib/routes.ts
@@ -6,16 +6,39 @@ import * as Router from 'koa-router';
 import { Request } from 'koa';
 import * as chalk from 'chalk';
 import * as glob from 'glob-promise';
-import { join } from 'path';
+import { join, relative } from 'path';
 
 const router = new Router();
 
 router.get('/', renderWrapper);
 
 async function renderWrapper(ctx: any, next: any) {
-  const jsFiles = await glob(join(process.env.PWD, '**', '*.js'));
+  const jsFiles = await glob(join(process.cwd(), '**', '*.js'), {
+    ignore: [
+      join(process.cwd(), 'node_modules', '**', '*')
+    ]
+  });
+
+  let d3Version: string | boolean;
+
+  try {
+    d3Version = ctx.cli.d3Version;
+  } catch (e) {
+    d3Version = false;
+  }
+
+  const d3Reg = /d3(?:-[0-9.]+)?(?:\.min)?\.js/;
+
+  const mapped = jsFiles.filter(file => !d3Reg.test(file)) // Filter out D3
+    .map(file => relative(process.cwd(), file));
+
+  const d3Path = jsFiles.filter(file => d3Reg.test(file))
+    .map(file => relative(process.cwd(), file)).shift();
+
   await ctx.render('wrapper', {
-    js: jsFiles,
+    js: mapped,
+    d3_path: d3Path,
+    d3_version: d3Version,
   });
 }
 
