460a30c4289084c1fa529bdaba51c06ea800de42
Ændrew Rininsland <andrew.rininsland@ft.com>
2016-08-23 14:31:03 +0100

    Initial for Framery

--- lib/index.ts
+++ lib/index.ts
@@ -4,21 +4,44 @@
  * 2016 Ændrew Rininsland
  */
 
-// Save your local vars in .env for testing. DO NOT VERSION CONTROL `.env`!.
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') require('dotenv').config();
-
 import * as Koa from 'koa';
 import * as bodyParser from 'koa-bodyparser';
+import * as views from 'koa-views';
+import * as mount from 'koa-mount';
+import * as serve from 'koa-static';
 import * as chalk from 'chalk';
+import * as convert from 'koa-convert';
+import * as koaBS from 'koa-browser-sync';
+import { resolve } from 'path';
 
 import router from './routes';
 
 const app = new Koa();
 const port = process.env.PORT || 5555;
+const viewsPath = resolve(__dirname, '..', '/views');
+const browserSync = convert(koaBS({
+  init: true,
+  logLevel: 'silent',
+  files: resolve(process.env.PWD, '**/*'),
+  notify: true,
+  open: true,
+  ui: true,
+  ghostMode: true,
+}));
 
-app.use(bodyParser())
-   .use(router.routes())
-   .use(router.allowedMethods());
+
+app.use(browserSync)
+  .use(bodyParser())
+  .use(views(viewsPath, {
+    extension: 'njk',
+    map: {
+      html: 'nunjucks'
+    }
+  }))
+ .use(serve(resolve(__dirname, '..', 'node_modules/', 'graphic-frame')))
+ .use(mount('project/', process.env.PWD))
+ .use(router.routes())
+ .use(router.allowedMethods());
 
 app.listen(port, () => console.log(chalk.black.bgGreen.bold(`Listening on port ${port}`)));
 
--- lib/routes.ts
+++ lib/routes.ts
@@ -3,26 +3,20 @@
  */
 
 import * as Router from 'koa-router';
-import {Request} from 'koa';
+import { Request } from 'koa';
 import * as chalk from 'chalk';
+import * as glob from 'glob-promise';
+import { join } from 'path';
 
 const router = new Router();
 
-/**
- * Index page. Currently doesn't do anything. ¯\_(ツ)_/¯
- */
-router.get('/', async (ctx, next) => {
-  await next();
-  ctx.body = '(This page intentionally left blank)';
-  ctx.status = 200;
-});
+router.get('/', renderWrapper);
+
+async function renderWrapper(ctx: any, next: any) {
+  const jsFiles = await glob(join(process.env.PWD, '**', '*.js'));
+  await ctx.render('wrapper', {
+    js: jsFiles,
+  });
+}
 
 export default router;
-
-interface IKoaRequestWithBody extends Router.IRouterContext {
-  request: IKoaBodyParserRequest;
-}
-
-interface IKoaBodyParserRequest extends Request {
-  body: any;
-}
